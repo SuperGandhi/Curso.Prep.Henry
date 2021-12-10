9934e86e0015c284f1ecc54a9c3bf6ff917104d0
SuperGandhi <patrickborjabueno@hotmail.com>
2021-12-10 15:28:52 -0500

    advance homework5

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,14 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var gato = {
+    nombre: nombre,
+    edad: edad,
+    meow: function () {
+      return "Meow!";
+    }
+  };
+  return gato;
 }
 
 
@@ -14,42 +22,63 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  var propiedad = property;
+  objeto[propiedad] = null;
+  return objeto;
 }
+
 
 function invocarMetodo(objeto, metodo) {
   // "metodo" es una cadena que contiene el nombre de un método (funcion) en el objeto
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  var metodoNombre = metodo;
+  objeto[metodoNombre]();
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-
+  objetoMisterioso.numeroMisterioso *= 5;
+  return objetoMisterioso.numeroMisterioso;
 }
-
 function eliminarPropiedad(objeto, unaPropiedad) {
-  // Elimina la propiedad de objeto cuyo nombre está pasado por el parametro unaPropiedad 
+  // Elimina la propiedad de objeto cuyo nombre está pasado por el parametro unaPropiedad
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
-}
+  var objeto = objeto;
+  var propiedad = unaPropiedad;
+  delete objeto[propiedad];
+  return objeto;
+  }
 
 function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-
+  var usuario = {
+    nombre: nombre,
+    email: email,
+    password: password
+  };
+  return usuario;
 }
 
 function tieneEmail(usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  var eMail = usuario.email; {
+    if (eMail == undefined) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
-
 
 
 function tienePropiedad(objeto, propiedad) {
@@ -57,6 +86,13 @@ function tienePropiedad(objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  var objeto = objeto;
+  var propiedad = propiedad;
+  if (objeto[propiedad] == undefined) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function verificarPassword(usuario, password) {
@@ -64,6 +100,14 @@ function verificarPassword(usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
+  var usuario = usuario;
+  var password = password;
+  if (usuario.password == password) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 function actualizarPassword(usuario, nuevaPassword) {
